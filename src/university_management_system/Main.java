2e93cb76e659f1295489ff6b44980adec8e347ad
Dulana23 <dulanaobapitiya@gmail.com>
2026-09-13 17:23:01 +0530

    Create Display Message

---
+++ src/university_management_system/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university_management_system;
+
+/**
+ *
+ * @author dulan
+ */
+public class Main {
+     
+    static void displayWelcomeMessage(){
+         System.out.println("===== UNIVERSITY STUDENT MANAGEMENT SYSTEM =====");
+         System.out.println("");
+    }
+    
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        loadMainMenu();
+    }
+}
